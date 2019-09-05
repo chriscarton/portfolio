6f13adc649032e2671bc0b2bbd4dbc324f0c2ae6
Chris <chriscarton@live.fr>
2019-09-05 11:24:04 +0200

    La mise en page devient plutôt correcte, reste à gérer le responsive

--- index.php
+++ index.php
@@ -4,8 +4,14 @@ require_once('projects.php');
 require_once('header.php');
 ?>
 	<header id="Header">
-		<h1>Chris Carton</h1>
-		<h2>Portfolio / Designer, Développeur Web</h2>
+		<div class="inblock">
+			<h1>
+				<a href="/portfolio">
+					Chris Carton
+				</a>
+			</h1>
+			<h2>Portfolio / Designer, Développeur Web</h2>
+		</div>
 	</header>
 	<div id="Content">
 		<?php 
--- listing.php
+++ listing.php
@@ -1,82 +1,78 @@
-<section id="templateGrid1">
-	<div class="section-intro">
-		<h1>Projets en tant qu'indépendant (auto-entreprise)</h1>
-	</div>
-	<div class="grid">	
-		<?= project($projects['esadvalenciennes']); ?>
-		
-		<div class="right">
-			<?= project($projects['stephenmaas']); ?>
-			<?= project($projects['petropelik']); ?>
+<div id="firstPart">
+	<section id="templateGrid1">
+		<div class="section-intro">
+			<h1>Projets en tant qu'indépendant (auto-entreprise)</h1>
 		</div>
-	</div>
-</section>
-<section class="ban" id="templateGrid2">
-	<div class="grid">
-		<?php
-		echo project($projects['placeauxcartons']); 
-		?>
-		<div class="subgrid">
-			<div class="info">
-				<p>Les deux essais suivants ont conduit à l'élaboration de ce système de site <i>one page</i>.</p>
+		<div class="grid">	
+			<?= project($projects['esadvalenciennes']); ?>
+			
+			<div class="right">
+				<?= project($projects['stephenmaas']); ?>
+				<?= project($projects['petropelik']); ?>
 			</div>
-			<?php 
-			echo concept([
-				'title'=>'Petit vélo',
-				'url'=>'https://chris-carton.com/concepts/petitvelo',
-				'cover'=>'img/concepts/petitvelo.png'
-			]);
-			echo concept([
-				'title'=>'Open mind',
-				'url'=>'https://chris-carton.com/concepts/openmind',
-				'cover'=>'img/concepts/openmind.png'
-			]);
+		</div>
+	</section>
+	<section class="ban" id="templateGrid2">
+		<div class="grid">
+			<?php
+			echo project($projects['placeauxcartons']); 
+			?>
+			<div class="subgrid">
+				<div class="info">
+					<p>Les deux essais suivants ont conduit à l'élaboration de ce système de site <i>one page</i>.</p>
+				</div>
+				<?php 
+				echo concept([
+					'title'=>'Petit vélo',
+					'url'=>'https://chris-carton.com/concepts/petitvelo',
+					'cover'=>'img/concepts/petitvelo.png'
+				]);
+				echo concept([
+					'title'=>'Open mind',
+					'url'=>'https://chris-carton.com/concepts/openmind',
+					'cover'=>'img/concepts/openmind.png'
+				]);
+				?>
+			</div>
+		</div>
+	</section>
+	<section class="ban" id="templateGrid3">
+		<div class="grid">
+			<?php
+			echo project($projects['emmanuelprin']);
+			echo project($projects['assuranceslewis']); 
+			echo project($projects['abcnatation']); 
 			?>
 		</div>
-	</div>
-</section>
-<section class="ban" id="templateGrid3">
-	<div class="grid">
-		<?php
-		echo project($projects['emmanuelprin']);
-
-		echo project($projects['assuranceslewis']); 
-
-		//Screenshooter sur...
-		//http://localhost/abcnatation/
-		//Mais il y a des bugs alors bon...
-		echo project($projects['abcnatation']); 
-		?>
-	</div>
-</section>
-<!-- Je suis en train d'oublier margot -->
-<!-- Fuckin' hell -->
-<section id="templateGrid4">
-	<div class="grid">
-		<?php 
-		echo project($projects['margot']); 
-		echo project($projects['chriscarton']); 
-		?>
-	</div>
-</section>
-<section id="templateGrid5">
-	<div class="section-intro">
-		<h1>Projets en agence (Les Sentinelles du Web)</h1>
-		<p>Un peu de blabla</p>
-	</div>
-	<div class="grid">
-		<?php 
-		echo project($projects['popeo']); 
-		echo project($projects['funster']); 
-		echo project($projects['femmesdebretagne']); 
-		echo project($projects['artbox']); 
-		echo project($projects['blogcolony']); 
-		echo project($projects['gtliens']); 
-		echo project($projects['handi']); 
-		echo project($projects['toprhu']); 
-		echo project($projects['bzh']); 
-		echo project($projects['trezulien']); 
-		echo project($projects['festimove']); 
-		?>
-	</div>
-</section>
+	</section>
+	<section class="ban" id="templateGrid4">
+		<div class="grid">
+			<?php 
+			echo project($projects['margot']); 
+			echo project($projects['chriscarton']); 
+			?>
+		</div>
+	</section>
+</div>
+<div id="secondPart">
+	<section id="templateGrid5">
+		<div class="section-intro">
+			<h1>Projets en agence (Les Sentinelles du Web)</h1>
+		</div>
+		<div class="grid">
+			<?php 
+			echo project($projects['popeo']); 
+			echo project($projects['funster']); 
+			echo project($projects['femmesdebretagne']); 
+			echo project($projects['artbox']); 
+			echo project($projects['blogcolony']); 
+			echo project($projects['gtliens']); 
+			echo project($projects['handi']); 
+			echo project($projects['toprhu']); 
+			echo project($projects['bzh']); 
+			echo project($projects['trezulien']); 
+			echo project($projects['festimove']); 
+			?>
+		</div>
+	</section>
+</div>
